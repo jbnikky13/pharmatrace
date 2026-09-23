5b697cb2ada541397501cb9820840e6a21cec974
Nichole Godam <79281031+jbnikky13@users.noreply.github.com>
2026-09-23 18:55:47 +0100

    Allow any connected wallet to register PharmaTrace batches

--- pharmatrace/contracts/PharmaTrace.sol
+++ pharmatrace/contracts/PharmaTrace.sol
@@ -68,7 +68,7 @@ contract PharmaTrace {
         string calldata manufactureDate,
         string calldata expiryDate,
         uint256 quantity
-    ) external onlyRegistrar returns (bytes32 batchKey) {
+    ) external returns (bytes32 batchKey) {
         require(bytes(batchId).length > 0, "EMPTY_BATCH_ID");
         require(quantity > 0, "INVALID_QUANTITY");
         batchKey = keccak256(bytes(batchId));
