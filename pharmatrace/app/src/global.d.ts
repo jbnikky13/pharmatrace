4008cd202326dbc7b0b0a58c43287d5d81c9328b
Nichole Godam <79281031+jbnikky13@users.noreply.github.com>
2026-09-23 11:05:47 +0100

    Complete EIP-1193 wallet typing

--- pharmatrace/app/src/global.d.ts
+++ pharmatrace/app/src/global.d.ts
@@ -1,8 +1,11 @@
 export {};
+
 declare global {
   interface Window {
     ethereum?: {
-      request: (args: { method: string; params?: unknown[] }) => Promise<any>;
+      request: (args: { method: string; params?: unknown[] | object }) => Promise<unknown>;
+      on?: (event: string, listener: (...args: unknown[]) => void) => void;
+      removeListener?: (event: string, listener: (...args: unknown[]) => void) => void;
     };
   }
 }
