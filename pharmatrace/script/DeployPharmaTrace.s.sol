62d440eb052bd166ffd5dd95fa3fbcea1a93ca82
Nichole Godam <79281031+jbnikky13@users.noreply.github.com>
2026-09-23 15:10:31 +0100

    Harden Arc Mainnet PharmaTrace deployment script

--- pharmatrace/script/DeployPharmaTrace.s.sol
+++ pharmatrace/script/DeployPharmaTrace.s.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
 import "forge-std/Script.sol";
 import "../contracts/PharmaTrace.sol";
 
 contract DeployPharmaTrace is Script {
     address constant ARC_USDC = 0x3600000000000000000000000000000000000000;
+
     function run() external returns (PharmaTrace deployed) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         deployed = new PharmaTrace(ARC_USDC);
         vm.stopBroadcast();
+        console2.log("PharmaTrace deployed:", address(deployed));
     }
 }
